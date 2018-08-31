7917259be30b391f42a868831b2d11c840f5ec06
stanley.lin <stanley.lin@quest.com>
2018-08-31 13:08:12 +0800

    project init.

---
+++ src/main/java/com/example/eurekaclientdemo/EurekaClientDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.eurekaclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientDemoApplication.class, args);
+	}
+}
